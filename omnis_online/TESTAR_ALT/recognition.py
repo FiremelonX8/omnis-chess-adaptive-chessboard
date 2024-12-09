5736bf28ec4eac51b132501de0ab9ea0caf70d23
FiremelonX8 <twitteranotherbrick@gmail.com>
2024-12-09 13:54:49 -0300

    some punctual changes in the test code

--- omnis_online/TESTAR_ALT/recognition.py
+++ omnis_online/TESTAR_ALT/recognition.py
@@ -8,10 +8,13 @@ class Reconhecimento:
     def validar_jogada(self, jogada):
         try:
             partes = jogada.split()
-            if len(partes) == 2:
-                self.jogada_f = partes[0] + partes[1]
-                return self.jogada_f
-            else:
+            for j in range(len(partes)):
+                self.jogadas_f += partes[j]
+
+            '''if len(partes) == 2:
+                self.jogada_f = partes[0] + partes[1]'''
+            
+            if len(self.jogadas_f.split()) > 1:
                 raise ValueError("Jogada incompleta.")
         except Exception as e:
             print(f"Erro na validação: {e}")
@@ -21,9 +24,9 @@ class Reconhecimento:
         while True:
             with sr.Microphone() as mic:
                 self.rec.adjust_for_ambient_noise(mic)
-                print("Fale o movimento (e.g., 'e2 e4'):")
+                print("Fale o movimento (ex: 'e2 e4'):")
                 try:
-                    audio = self.rec.listen(mic)
+                    audio = self.rec.listen(mic, timeout=6)
                     jogada = self.rec.recognize_google(audio, language='pt-BR').lower()
                     jogada_valida = self.validar_jogada(jogada)
                     if jogada_valida:
@@ -34,3 +37,5 @@ class Reconhecimento:
                     print("Não entendi. Por favor, repita.")
                 except sr.RequestError as e:
                     print(f"Erro no serviço de reconhecimento: {e}")
+                except sr.WaitTimeoutError:
+                    print(f"Você não falou nada em 8 segundos")
